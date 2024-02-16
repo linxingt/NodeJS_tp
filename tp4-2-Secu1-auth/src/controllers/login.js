f3dc7e463a52f5137f3b9af8d3671839e34c9f7b
linxingt <xingtonglin011016@gmail.com>
2024-02-16 20:30:26 +0100

    etape 3 half
    Générer les clés de chiffrements avec openssl en ECDSA compatible avec la norme JWT. Vous
    les placerez dans le dossier .ssl du projet.
     ̈ Compléter l’enregistrement du plugin fasitfyJwt dans le fichier src/plugins/jwt.js.
     ̈ Compléter le handler addUser() dans le cas où il s’agit d’un nouveau compte utilisateur dans le
    fichier src/controllers/login.js.
     ̈ Écrire le handler loginUser() dans le fichier src/controllers/login.js pour signer et renvoyer un
    jeton pour un utilisateur valide.

--- tp4-2-Secu1-auth/src/controllers/login.js
+++ tp4-2-Secu1-auth/src/controllers/login.js
@@ -1,7 +1,12 @@
 import {createHash} from "node:crypto"
+import jwtPlugin from "../plugins/jwt.js"
+import Fastify from "fastify"
 
 const users = []    // Simule BDD pour le stockage des utilisateurs
 const role = ['admin', 'utilisateur']
+
+const fastify = Fastify({ logger: true });
+jwtPlugin(fastify);
 
 export const addUser = async (req, res) => {
     const {email, password} = req.body
@@ -16,10 +21,41 @@ export const addUser = async (req, res) => {
     }
 
     // A compléter
+
+    const newUser = {
+        email,
+        password: hashedPassword,
+        role: role[Math.floor(Math.random() * role.length)],
+    };
+
+    users.push(newUser);
+
+    res.status(200).send({
+        message: "Utilisateur enregistré avec succès",
+        newUser,
+    });
 }
 
 export const loginUser = async function (req, res) {
 
     // A compléter
+    const {email, password} = req.body
+    const hashedPassword = createHash("sha256").update(password).digest().toString("hex")
 
+    let user = users.find((u) => u.email === email && u.password === hashedPassword)
+    if (!user) {
+        res.status(401).send({
+            message: "Utilisateur non-identifié",
+        });
+    }
+    // private key
+    const token = await req.jwtSign({
+        email: user.email,
+        role: user.role
+    });
+
+    res.status(200).send({
+        message: "Utilisateur identifié avec succès",
+        token,
+    });
 }
--- tp4-2-Secu1-auth/src/plugins/jwt.js
+++ tp4-2-Secu1-auth/src/plugins/jwt.js
@@ -1,13 +1,25 @@
 import fp from 'fastify-plugin'
 import fastifyJwt from "@fastify/jwt";
-
+import { readFileSync } from "node:fs";
 export default fp(async function (app, opts) {
 
     app.register(fastifyJwt, {
+        Secret : {
+            private : readFileSync ('../../.ssl/ec_private.pem'),
+            public : readFileSync ('../../.ssl/ec_public.pem')
+        } ,
         sign: {
             algorithm: 'ES256',
             issuer: 'info.iutparis.fr'
         },
     })
+
+    app.listen(3000, (err, address) => {
+        if (err) {
+            app.log.error(err);
+            app.exit(1);
+        }
+        app.log.info(`Server is listening on ${address}`);
+    });
 
 })
